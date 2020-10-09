ed9c3f6a158135e45995ab885004e6f5af9929c1
Manav Jain <manav.mj.jain@gmail.com>
2020-10-10 00:52:21 +0530

    fix: pluralize comment in doubts show.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    User.find_by(id: session[:user_id])
+    if session[:user_id]
+      User.find_by(id: session[:user_id])
+    end
   end
 
   def logged_in?
