d29f4e745c0ddb946c1aff163a2548f23adadef7
Manav Jain <manav.mj.jain@gmail.com>
2020-10-10 00:59:00 +0530

    galti

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
-    end
+    User.find_by(id: session[:user_id])
   end
 
   def logged_in?
